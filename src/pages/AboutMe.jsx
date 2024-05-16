66dafe628d3a9e7423575651d4c746adcbc35693
Cristi Zubcu <zubcu30@gmail.com>
2024-05-16 17:33:26 +0200

    css about me

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -12,12 +12,16 @@ export function AboutMe() {
     ];
 
     return (
-        <div className="aboutme cambo-regular">
+        <div className="aboutme bubblegum-sans-regular">
             <h1>About me</h1>
             <h2>Hello! I am Lesley.</h2>
-            <h3>I am a <strong>web developer/designer</strong> with a focus on <strong>UX design and research</strong>. I hold a Bachelor's degree in communication science, which helps me create engaging digital experiences.</h3>
-            <h3>I am passionate about the intersection of technology and human interaction. I enjoy creating digital solutions that are visually appealing, user-friendly, and immersive.</h3>
-            <h1>Skills</h1>
+            <h3>
+                I am a <strong>web developer/designer</strong> with a focus on <strong>UX design and research</strong>. 
+                I hold a Bachelor's degree in communication science, which helps me create engaging digital experiences.
+                <br /><br />I am passionate about the intersection of technology and human interaction. 
+                I enjoy creating digital solutions that are visually appealing, user-friendly, and immersive.</h3>
+
+            <h1>My Skills</h1>
             <div className="skills-container">
                 {skills.map((skill, index) => (
                     <Tilt 
