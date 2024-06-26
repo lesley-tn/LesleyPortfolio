a7348b58d96ddd697dec8b3b139726c2c3e8ee77
Lesley Huang <lesley.znh@gmail.com>
2024-06-26 16:31:06 +0800

    update MobileDetector

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,24 @@ import App from './App';
 import "./styles.css";
 import { Overlay } from './pages/Overlay';
 
-function MobileDetector (children) {
-  const isMobile = /iPhone|iPad|iPod|Anroid/i.test(navigator.userAgent);
+const MobileDetector = ({ children }) => {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
   if (isMobile) {
     return (
-      <div style={{ textAlign: 'center', marginTop:'25vh', marginLeft:'8px', marginRight:'8px'}}>
-        <h2 style={{ fontSize: '3vh'}}>Site not available on mobile devices.
-        <br/><br/>
-        Please visit this site on a PC or laptop.</h2>
+      <div style={{ textAlign: 'center', marginTop: '25vh', marginLeft: '8px', marginRight: '8px' }}>
+        <h2 style={{ fontSize: '3vh' }}>
+          Site not available on mobile devices.
+          <br /><br />
+          Please visit this site on a PC or laptop.
+        </h2>
       </div>
-    )
-  };
+    );
+  }
 
   return <>{children}</>;
-}
+};
+
 createRoot(document.getElementById('root')).render(
   <MobileDetector>
     <App />
